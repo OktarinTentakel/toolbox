cc79029391caecdb4b7db5530c4c373c51367e85
sebastian@1337server.com <sebastian@1337server.com@f0ddc53c-4922-0117-0135-68c7fdccfae0>
2010-11-06 17:57:28 +0000

    - basic module loading structure - start of testcases

---
+++ trunk/index.php
@@ -0,0 +1,17 @@
+﻿<?php
+
+require_once 'toolbox/ToolBox.class.php';
+
+?>
+
+<!doctype html>
+
+<html>
+	<head>
+		<title>ToolBox Testcases</title>
+	</head>
+	<body>
+		<h1>ToolBox Testcases</h1>
+		<?=ToolBox::download()->requestFile('test')?>
+	</body>
+</html>
