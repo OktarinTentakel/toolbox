845648fba59992f4863a298697b9399485369cd1
sebastian.schlapkohl <sebastian.schlapkohl@f0ddc53c-4922-0117-0135-68c7fdccfae0>
2011-02-05 21:47:15 +0000

    - added generation-module with uniqueMd5Id

---
+++ trunk/toolbox/ToolBoxModule.generation.class.php
@@ -0,0 +1,27 @@
+<?php
+
+//--|INCLUDES----------
+
+require_once 'ToolBoxModule.absclass.php';
+
+
+
+//--|CLASS----------
+
+class ToolBoxModuleGeneration extends ToolBoxModule {
+
+	// ***
+	public function __construct($moduleName, $addedArgs){
+		parent::__construct($moduleName, $addedArgs);
+	}
+	// ***
+	
+	
+	
+	public function uniqueMd5Id($seed = null){
+		return md5(uniqid(is_null($seed) ? mt_rand() : $seed, true));
+	}
+	
+}
+
+?>
